9f3052e648ca1d9e7feb9999e6a0524a0b4ed45c
German G <ideaoriginal2000@gmail.com>
2022-07-29 20:41:52 -0300

    .

--- send-opus.php
+++ send-opus.php
@@ -4,6 +4,7 @@
     $apellido = $_POST["apellido-opus"];
     $correo = $_POST["correo-opus"];
     $mensaje = $_POST["mensaje-opus"];
+    $asunto = "Enviado desde la Web de Proteina y Cultura para OpusOperaPop";
     $contenido = "Nombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
     mail($destino,"contacto",$contenido);
     header("Location:redirect.html");
--- send.php
+++ send.php
@@ -4,6 +4,7 @@
     $apellido = $_POST["apellido"];
     $correo = $_POST["correo"];
     $mensaje = $_POST["mensaje"];
+    $asunto = "Enviado desde la Web de Proteina y Cultura para la Fundación";
     $contenido = "Nombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
     mail($destino,"contacto",$contenido);
     header("Location:redirect.html");
