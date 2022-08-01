e19f9d85c29f94fba33e8195b25281ed0a428aac
German G <ideaoriginal2000@gmail.com>
2022-08-01 09:46:29 -0300

    .

--- send-opus.php
+++ send-opus.php
@@ -1,11 +1,11 @@
 <?php
-    $destino = "info@opusoperapop.com.ar";
+    $destino = "info@proteinaycultura.org.ar";
     $nombre = $_POST["nombre-opus"];
     $apellido = $_POST["apellido-opus"];
     $correo = $_POST["correo-opus"];
     $mensaje = $_POST["mensaje-opus"];
     $asunto = "Enviado desde la Web de Proteina y Cultura para OpusOperaPop";
-    $contenido = "Nombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
+    $contenido = $asunto."\nNombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
     mail($destino,"contacto",$contenido);
     header("Location:redirect.html");
 ?>
--- send.php
+++ send.php
@@ -5,7 +5,7 @@
     $correo = $_POST["correo"];
     $mensaje = $_POST["mensaje"];
     $asunto = "Enviado desde la Web de Proteina y Cultura para la Fundación";
-    $contenido = "Nombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
+    $contenido = $asunto."\nNombre: ".$nombre."\nApellido: ".$apellido."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
     mail($destino,"contacto",$contenido);
     header("Location:redirect.html");
 ?>
